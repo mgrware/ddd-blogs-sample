1678aae4dc24775ad53c77859edd844293763a56
Gilang Ramadan <mgrware@gmail.com>
2022-11-08 13:27:50 +0700

    remove unused code

--- lib/blogging/events/article_submitted.rb
+++ lib/blogging/events/article_submitted.rb
@@ -1,9 +1,5 @@
 
 module Blogging
   class ArticleSubmitted < Infra::Event
-    attribute :article_id, Infra::Types::UUID
-    attribute :article_number, Infra::Types::ArticleNumber
-    attribute :content, Infra::Types::String
-    attribute :title, Infra::Types::String
   end
 end
